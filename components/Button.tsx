781b9a8c0438ae5889f74eb954c2637251035df2
Mohamed Hegazy <48334725+MohamedHegazy2020@users.noreply.github.com>
2025-08-27 19:28:05 +0300

    Refactor Button component and integrate it into GetApp for improved UI and functionality

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,25 +1,27 @@
 import Image from "next/image";
 
-type ButtonProps ={
-  type: "button" | "submit" | "reset";
+type ButtonProps = {
+  type: "button" | "submit";
   title: string;
-  variant: "btn_white" | "btn_white_text" | "btn_green" | "btn_dark_green" | "btn_dark_green_outline" | "green" | "orange" | "gray";
-  fullWidth?: boolean;
   icon?: string;
-}
-const Button: React.FC<ButtonProps> = ({
-  type,
-  title,
-  variant,
-  fullWidth = false,
-  icon,
-}) => {
-    return <button type={type} className={`flexCenter gap-3 px-6 py-3 rounded-full border font-semibold ${fullWidth ? "w-full" : "w-fit"} ${variant}`}>
-        {icon && <Image src={icon} alt={title} width={24} height={24} />}
-        <label className="bold-16 whitespace-nowrap" >{title}</label>
-  </button>;
+  variant: string;
+  full?: boolean;
 };
 
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
+};
 
-
-export default Button
+export default Button;
